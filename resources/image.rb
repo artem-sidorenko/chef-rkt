163232a637acc2dd10d7b51f0b07c8ce04a9e7e4
Artem Sidorenko <artem@posteo.de>
2016-05-09 19:58:08 +0200

    Refactoring: helpers library for helper functions

--- resources/image.rb
+++ resources/image.rb
@@ -10,6 +10,8 @@
 # and at https://gitlab.com/artem-sidorenko/chef-rkt/blob/master/COPYRIGHT
 #
 
+require_relative '../libraries/helpers'
+
 property :image_url, String, name_property: true
 property :no_store, [TrueClass, FalseClass], default: false
 property :trust_keys_from_https, [TrueClass, FalseClass], default: false
@@ -25,18 +27,10 @@ action :create do
     Chef::Log.info("#{new_resource} already exists - nothing to do")
   else
     converge_by("#{new_resource} fetching image #{image_url}") do
-      cmd = "rkt fetch #{image_url}"
-      cmd += ' --no-store=true' if no_store
-      cmd += ' --trust-keys-from-https=true' if trust_keys_from_https
-      run_cmd = Mixlib::ShellOut.new(cmd).run_command
-      if run_cmd.error?
-        raise "#{new_resource} action create failed, following command line was called: #{cmd}
-              stdout:
-              #{run_cmd.stdout}
-              stderr:
-              #{run_cmd.stderr}
-              "
-      end
+      cmd_args = [image_url]
+      cmd_args << '--no-store=true' if no_store
+      cmd_args << '--trust-keys-from-https=true' if trust_keys_from_https
+      rkt_run_cmd('fetch', cmd_args, 'create')
     end
   end
 end
@@ -47,29 +41,7 @@ action :delete do
     Chef::Log.info("#{new_resource} doesn't exists - nothing to do")
   else
     converge_by("#{new_resource} deleting image id #{image_id}") do
-      cmd = "rkt image rm #{image_id}"
-      run_cmd = Mixlib::ShellOut.new(cmd).run_command
-      if run_cmd.error?
-        raise "#{new_resource} action delete failed, following command line was called: #{cmd}
-              stdout:
-              #{run_cmd.stdout}
-              stderr:
-              #{run_cmd.stderr}
-              "
-      end
+      rkt_run_cmd('image rm', [image_id], 'delete')
     end
   end
-end
-
-def rkt_image_name_to_id(image_name)
-  cmd = 'rkt image list'
-  cmd += ' --no-legend=true --fields=id,name'
-  run_cmd = Mixlib::ShellOut.new(cmd).run_command
-  raise "#{new_resource} image check failed, following command line was called: #{cmd}" if run_cmd.error?
-  images = {}
-  run_cmd.stdout.split("\n").each do |line|
-    values = line.split
-    images[values[1]] = values[0]
-  end
-  images[image_name]
 end
